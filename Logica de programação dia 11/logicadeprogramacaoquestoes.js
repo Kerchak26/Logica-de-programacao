96fa6fccfab2f8e5c7115e8083c54e1d2bd0a750
Kerchak26 <124314219+Kerchak26@users.noreply.github.com>
2023-04-11 21:36:18 -0300

    Add files via upload

---
+++ Logica de programação dia 11/logicadeprogramacaoquestoes.js
@@ -0,0 +1,17 @@
+var A = 12;
+var B = 6;
+var C = -3;
+var D = parseFloat (1,5);
+var resultado = 2 * 12 % 3 - -3;
+console.log(resultado)
+var A = 12;
+var B = 6;
+var C = -3;
+var D = parseFloat (1,5);
+var resultado = -2 * -3 / 4
+console.log(resultado)
+var A = 12;
+var B = 6;
+var C = -3;
+var D = parseFloat (1,5);
+var resultado = Math.pow((20 % 3),3) + Math.pow(2,8) /2
